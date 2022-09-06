a210c273d1f9dcd5f2291bb73adcb8e31ced890b
Alex Vong <alexvong14@gmail.com>
2022-09-06 14:41:22 +0100

    Forgot to add comments

--- CSharp/FizzBuzz.cs
+++ CSharp/FizzBuzz.cs
@@ -10,21 +10,28 @@ namespace FizzBuzzCS
     {
         static void Main(string[] args)
         {
+            // Multiples and fizzbuzz strings
             string[] fizzBuzz = { "Fizz", "Buzz" };
             int[] multiples = { 3, 5 };
 
+            // Loop from 1 to 100
             for (int i = 0; i <= 100; i++)
             {
+                // Empty string 
                 string output = "";
                 for (int j = 0; j < multiples.Length; j++)
                 {
+                    // If a multiple
                     if (i % multiples[j] == 0)
                     {
+                        // Add the relevant string
                         output += fizzBuzz[j];
                     }
                 }
+                // If the string is empty it is not a multiple of anything
                 if (output == "")
                 {
+                    // Print the number
                     Console.WriteLine(i);
                 }
                 else
