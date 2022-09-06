ee767ceec1c4a5373f5b330436ecd0b167782652
Alex Vong <alexvong14@gmail.com>
2022-09-06 14:37:37 +0100

    C# Implementation

---
+++ CSharp/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzCS
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            string[] fizzBuzz = { "Fizz", "Buzz" };
+            int[] multiples = { 3, 5 };
+
+            for (int i = 0; i <= 100; i++)
+            {
+                string output = "";
+                for (int j = 0; j < multiples.Length; j++)
+                {
+                    if (i % multiples[j] == 0)
+                    {
+                        output += fizzBuzz[j];
+                    }
+                }
+                if (output == "")
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
