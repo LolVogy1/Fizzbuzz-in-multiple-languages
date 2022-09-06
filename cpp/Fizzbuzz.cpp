5c772ce5224b766f0bead7891008c1224ea2dd8e
Alex Vong <alexvong14@gmail.com>
2022-09-06 13:44:32 +0100

    Add pseudocode and C++ implementation

---
+++ cpp/Fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string fizzBuzz[] = { "Fizz", "Buzz" };
+	int multiples[] = { 3, 5 };
+
+	//from 1 to 100
+	for (int i = 0; i <= 100; i++) {
+		std::string output = "";
+		//for each multiple
+		for (int j = 0; j <= (sizeof(multiples) / sizeof(*multiples)) - 1; j++) {
+			//if a multiple of j
+			if (i % multiples[j] == 0) {
+				output = output.append(fizzBuzz[j]);
+			}
+		}
+		//if not a multiple of the given numbers
+		if (output == "") {
+			std::cout << i << "\n";
+		}
+		else {
+			std::cout << output << "\n";
+		}
+	}
+}
